3eaa6d148ca698a2614d0701b8c167f52fac6aa4
Rodrigo Pereira <66509029+rodrigo-222@users.noreply.github.com>
2024-05-29 15:37:55 -0300

    Update hagman_nlp.py

--- hangmania/hagman_nlp.py
+++ hangmania/hagman_nlp.py
@@ -1,3 +1,4 @@
+# Importação das bibliotecas necessárias
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
@@ -10,38 +11,43 @@ import pickle
 def word_to_binary(word, alphabet):
     return [int(letter in word) for letter in alphabet]
 
-# Ler o arquivo Excel
+# Ler o arquivo Excel contendo as palavras sem acentos
 df = pd.read_excel("br-sem-acentos.xlsx", engine='openpyxl')
 
-# Definir o alfabeto
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] # Lista de todas as letras do alfabeto
+# Definir o alfabeto completo
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-# Converter palavras em características binárias
+# Converter palavras em características binárias usando a função word_to_binary
 X = df.iloc[:, 0].apply(lambda x: word_to_binary(x.lower(), alphabet)).tolist()
 
 # Criar os rótulos com base nas frequências de cada letra nas palavras
 num_rows = len(df)
-print(num_rows)
-y = np.zeros((num_rows, 26))  # Matriz de zeros com num_rows linhas e 26 colunas
+print(num_rows)  # Imprime o número total de linhas no DataFrame
+y = np.zeros((num_rows, 26))  # Inicializa uma matriz de zeros com dimensões adequadas
 for index, row in df.iterrows():
     for letter in alphabet:
-        if letter in row['Palavra'].lower():  # Verifica se a letra está na palavra
+        if letter in row['Palavra'].lower():  # Verifica se a letra está presente na palavra
             y[index, alphabet.index(letter)] = 1  # Marca a coluna correspondente como 1
 
-print(X)
+print(X)  # Imprime o vetor de características binárias X para verificação
+
 # Dividir os dados em conjuntos de treinamento e teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Treinar um modelo para cada saída usando LogisticRegression
+# Treinar um modelo para cada saída usando RandomForestClassifier
 models = []
 for i in range(y_train.shape[1]):
     model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train[:, i])
-    models.append(model)
+    model.fit(X_train, y_train[:, i])  # Treina o modelo com os dados de treinamento
+    models.append(model)  # Armazena o modelo treinado
 
-# Envolver os modelos com MultiOutputClassifier
-multi_output_classifier = MultiOutputClassifier(RandomForestClassifier( random_state=42), n_jobs=-1)
-multi_output_classifier.fit(X_train, y_train)
+# Envolver os modelos individuais com MultiOutputClassifier para suporte a múltiplas saídas
+multi_output_classifier = MultiOutputClassifier(RandomForestClassifier(random_state=42), n_jobs=-1)
+multi_output_classifier.fit(X_train, y_train)  # Treina o classificador de múltiplas saídas
 
+# Nome do arquivo para salvar o modelo treinado
+filename = "multi_output_random_forest_Classifier.pkl"
+
+# Salvar o modelo treinado usando pickle
 with open(filename, 'wb') as file:
-    pickle.dump(multi_output_classifier, file)
+    pickle.dump(multi_output_classifier, file)  # Serializa e salva o modelo treinado
